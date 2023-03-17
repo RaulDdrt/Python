823bd114ce3cd8349bc9e127b6655fff07770edc
RaulDdrt <rauldiazdrt@gmail.com>
2023-03-17 15:52:43 +0000

    segundo commit

---
+++ input.py
@@ -0,0 +1,8 @@
+# print("Welcome to the greeter program")
+# name = input("Enter your name")
+# print("Greetins " + name)
+
+print("Calculator program")
+numero1 = input("first numer")
+numero2 = input("second numer")
+print( int(numero1) + int(numero2))
